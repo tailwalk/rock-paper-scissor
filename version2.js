85c7552aee609b01053b274605c3b21172e72cdf
Tailwalk <130933543+tailwalk@users.noreply.github.com>
2023-06-09 19:34:09 -0400

    added more interactivity between buttons and tally

--- version2.js
+++ version2.js
@@ -1,29 +1,71 @@
 const scoreBoard = document.querySelector('.scores');
-const results = document.querySelector('.results');
+const result = document.querySelector('.result > p');
 const rock = document.getElementById('r')
 const paper = document.getElementById('p')
 const scissors = document.getElementById('s')
-const userScore = 0
-const compScore = 0
-const userTally = document.getElementById('#user')
-const compTally = document.getElementById('#comp')
+let userScore = 0
+let compScore = 0
+const userTally = document.getElementById('user-score')
+const compTally = document.getElementById('comp-score')
+
+function getCompChoice(user, comp){
+    const choices =["r", "p", "s"];
+    const botChoice = choices[Math.floor(Math.random() * choices.length)]
+    return botChoice
+}
+function win(userChoice, compChoice){
+    userScore++
+    console.log(userScore)
+    console.log('win')
+    userTally.innerHTML = userScore
+    compTally.innerHTML = compScore
+    result.innerHTML = `${userChoice} beats ${compChoice}`
+
+}
+function lose(){
+    compScore++
+    console.log(compScore)
+    userTally.innerHTML = userScore
+    compTally.innerHTML = compScore
+    
+}
+function tie(){
+    userTally.innerHTML = userScore
+    compTally.innerHTML = compScore
+    
+}
 
 function game(userChoice){
-    console.log('poop')
+    const compChoice = getCompChoice()
+ switch (userChoice + compChoice){
+    case "pr":
+    case "rs":
+    case "sp":
+    win(userChoice, compChoice) 
+        break;
+    case "rp":
+    case "ps":
+    case "sr":
+    lose(userChoice, compChoice)
+        break;
+    case "pp":
+    case "ss":
+    case "rr":
+    tie(userChoice, compChoice)
+        break;
+ }
 }
+game()
 
 function main(){
     rock.addEventListener('click', function(){
-        game('r')
-
+        game('r');
     })
     paper.addEventListener('click', function(){
-        game('p')
-
+        game('p');
     })
     scissors.addEventListener('click', function(){
-        game('s')
-
+        game('s');
     })
-}
-
+    }
+main()
